3605f1bf79a5a0c79760d7a3bb5d2ede146b13ce
Kopalmuhamad <muhamadkopal32@gmail.com>
2024-06-02 17:22:02 +0700

    31-slicing---make-a-home-page-responsive

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -16,6 +16,8 @@ import {
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
 
 interface IProductListData {
   id: number;
@@ -36,10 +38,10 @@ const Home = () => {
   console.log(data);
   return (
     <Layout>
-      <div className="w-full h-[440px] bg-gradient-to-r from-[#036DA1] via-[#3487AC] to-[#C6D6CE]">
-        <section className="container mx-auto h-full grid grid-cols-2 place-items-center">
-          <main className="flex flex-col items-start justify-start gap-4">
-            <h1 className="text-4xl font-semibold text-white">
+      <div className="w-full h-auto bg-gradient-to-r from-[#036DA1] via-[#3487AC] to-[#C6D6CE]">
+        <section className="container mx-auto h-full grid grid-cols-1 lg:grid-cols-2 place-items-center pt-8">
+          <main className="flex flex-col items-center justify-center lg:items-start lg:justify-start gap-4">
+            <h1 className="text-2xl lg:text-4xl font-semibold text-center lg:text-start text-white">
               Kesehatan dan Kebahagiaan untuk Sahabat Setia Anda
             </h1>
             <Link to={"/"} className={buttonVariants({ variant: "outline" })}>
@@ -52,34 +54,33 @@ const Home = () => {
         </section>
       </div>
       <section className="container mx-auto h-full flex items-start justify-center flex-col gap-y-8 py-6">
-        <header className="w-full flex items-start justify-between">
-          <div className="grid grid-cols-2 grid-rows-auto max-w-[200px] shadow-2xl rounded-md py-2 px-4 gap-x-4 gap-y-2">
-            <h1 className="col-span-2 row-start-1 text-lg font-semibold">
-              Category
-            </h1>
-            <Button
-              variant="ghost"
-              className="col-start-1 row-start-2 w-max h-max flex items-center justify-center flex-col gap-y-2"
-            >
-              <img
-                src="/public/assets/petshop-icon.png"
-                alt=""
-                className="w-[50px] ring-2 ring-border rounded-full"
-              />
-              <h3 className="font-regular">Pet Shop</h3>
-            </Button>
-            <Button
-              variant="ghost"
-              className="col-start-2 row-start-2 flex items-center justify-center flex-col gap-y-2 w-max h-max"
-            >
-              <img
-                src="/public/assets/check-up-icon.png"
-                alt=""
-                className="w-[50px] ring-2 ring-border rounded-full"
-              />
-              <h3 className="font-regular">Clinic</h3>
-            </Button>
-          </div>
+        <header className="w-full flex flex-col items-start sm:items-end justify-between gap-y-4 sm:flex-row">
+          <Tabs
+            defaultValue="account"
+            className="w-[400px] h-full flex items-start justify-start flex-col gap-y-2"
+          >
+            <h1 className="text-base font-semibold">Category</h1>
+            <TabsList>
+              <TabsTrigger
+                value="account"
+                className="flex items-center justify-center flex-col gap-y-[2px]"
+              >
+                <Avatar>
+                  <AvatarImage src="/public/assets/petshop-icon.png" />
+                </Avatar>
+                <h1>Pet Shop</h1>
+              </TabsTrigger>
+              <TabsTrigger
+                value="password"
+                className="flex items-center justify-center flex-col gap-y-[2px]"
+              >
+                <Avatar>
+                  <AvatarImage src="/public/assets/check-up-icon.png" />
+                </Avatar>
+                <h1>Clinic</h1>
+              </TabsTrigger>
+            </TabsList>
+          </Tabs>
           <Select>
             <SelectTrigger className="w-[180px]">
               <SelectValue placeholder="Default" />
