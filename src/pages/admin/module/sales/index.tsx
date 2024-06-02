24930589ece6322e2facac19f295a23b94faa708
Kopalmuhamad <muhamadkopal32@gmail.com>
2024-06-02 13:28:37 +0700

    23-slicing-sales-history-admin

---
+++ src/pages/admin/module/sales/index.tsx
@@ -0,0 +1,14 @@
+import Layout from "@/components/layout";
+import DataTable from "./data-table";
+
+function Sales() {
+  return (
+    <Layout>
+      <div className="container mx-auto py-10">
+        <DataTable />
+      </div>
+    </Layout>
+  );
+}
+
+export default Sales;
