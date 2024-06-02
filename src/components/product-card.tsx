5f4c5efdf8bc7999a669d8d00d1f7c9193f5e978
Kopalmuhamad <muhamadkopal32@gmail.com>
2024-06-02 23:18:34 +0700

    Buat component card product

---
+++ src/components/product-card.tsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+import { Card, CardContent, CardFooter, CardHeader } from "./ui/card";
+import { IProductListData } from "@/utils/apis/products";
+
+interface IProductCart {
+  data: IProductListData;
+}
+
+const ProductCard = ({ data }: IProductCart) => {
+  return (
+    <Link to={"/"} key={data.id}>
+      <Card>
+        <CardHeader>
+          <img
+            src={data.product_picture}
+            alt=""
+            className="rounded-sm h-[300px] object-cover"
+          />
+        </CardHeader>
+        <CardContent>
+          <h1 className="text-base font-semibold">{data.product_name}</h1>
+        </CardContent>
+        <CardFooter>
+          <h3>Rp. {data.price}</h3>
+        </CardFooter>
+      </Card>
+    </Link>
+  );
+};
+
+export default ProductCard;
--- src/pages/home/ProductList.tsx
+++ src/pages/home/ProductList.tsx
@@ -1,11 +1,5 @@
-import { Link } from "react-router-dom";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-} from "@/components/ui/card";
 import { IProductListData } from "../../utils/apis/products";
+import ProductCard from "@/components/product-card";
 
 interface ProductListProps {
   data: IProductListData[];
@@ -15,23 +9,7 @@ const ProductList = ({ data }: ProductListProps) => {
   return (
     <main className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
       {data.map((item) => (
-        <Link to={"/"} key={item.id}>
-          <Card>
-            <CardHeader>
-              <img
-                src={item.product_picture}
-                alt=""
-                className="rounded-sm h-[300px] object-cover"
-              />
-            </CardHeader>
-            <CardContent>
-              <h1 className="text-base font-semibold">{item.product_name}</h1>
-            </CardContent>
-            <CardFooter>
-              <h3>Rp. {item.price}</h3>
-            </CardFooter>
-          </Card>
-        </Link>
+        <ProductCard data={item} />
       ))}
     </main>
   );
